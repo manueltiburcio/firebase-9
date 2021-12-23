870c6c793dc9485125636f171bbb9bad0b475e85
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2021-12-23 01:22:39 -0400

    Firebase Initialized

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,26 +7,43 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log('hello from index.js')\n\n//# sourceURL=webpack://getting-started-with-firebase-9/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\nconst firebaseConfig = {\n    apiKey: \"AIzaSyDxE3m--hATNFKweAbtMTxoN7CYDMwmEco\",\n    authDomain: \"fir-9-f0a43.firebaseapp.com\",\n    projectId: \"fir-9-f0a43\",\n    storageBucket: \"fir-9-f0a43.appspot.com\",\n    messagingSenderId: \"630417697355\",\n    appId: \"1:630417697355:web:5b0341e2c878cc6f663bce\"\n  };\n\n   Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(firebaseConfig)\n\n//# sourceURL=webpack://getting-started-with-firebase-9/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
